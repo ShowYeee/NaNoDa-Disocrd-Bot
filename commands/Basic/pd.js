5e08a528abaa1a0a8f6fc67899204eabc383202a
Mike <chaper1995@gmail.com>
2021-10-28 23:58:22 +0800

    Fix

--- commands/Basic/pd.js
+++ commands/Basic/pd.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: '人事部',
 	description: '人事部',
 	execute(message,Discord) {
-        var attachment = new Discord.MessageAttachment('./assests/image/pd.gif','gif');
+        var attachment = new Discord.MessageAttachment('./assests/image/pd.gif');
         message.channel.send(attachment); 
 	},
 };
