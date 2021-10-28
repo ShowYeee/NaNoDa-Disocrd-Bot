87ef410eea46c8c0b1cca75572700069e97889ee
Mike <chaper1995@gmail.com>
2021-10-28 23:53:44 +0800

    人事部

---
+++ commands/Basic/pd.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: '人事部',
+	description: '人事部',
+	execute(message,Discord) {
+        var attachment = new Discord.MessageAttachment('./assests/image/pd.gif','gif');
+        message.channel.send(attachment); 
+	},
+};
