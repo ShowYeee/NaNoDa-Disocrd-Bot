9471e1bb7dbafb909483a6c1aa7673aee9394813
Mike-Yu <chaper1995@gmail.com>
2022-05-18 10:16:25 +0800

    Update: anya

---
+++ commands/Basic/anya.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: '呵',
+	description: '呵',
+	execute(message,Discord) {
+        var attachment = new Discord.MessageAttachment('./assests/image/anya.jpg','jpg');
+        message.channel.send(attachment); 
+	},
+};
