21194ca959a5b36c748ab505516cca276932f81a
Mike-Chiu <chaper1995@gmail.com>
2022-05-25 23:17:01 +0800

    Fix: 確診占卜

--- commands/Basic/covid19.js
+++ commands/Basic/covid19.js
@@ -5,6 +5,7 @@ module.exports = {
         var num = Math.floor(Math.random()*2)+1;
         var path = "./assests/image/covid19/" + num + ".jpg"
         var attachment = new Discord.MessageAttachment(path);
+        message.reply('這是你的快篩結果：');
         message.channel.send(attachment); 
 	},
 };
